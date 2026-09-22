04c8a53a0d4b705f4e35e6c6b9cd87b2706dea5b
KITSBohdanRakov <Bohdan_rakov@epam.com>
2026-09-22 14:10:00 +0000

    Changed get resource endpoint content-type to audio/mpeg

--- resource-service/src/main/java/io/github/bohdanrakov/controllers/ResourceServiceController.java
+++ resource-service/src/main/java/io/github/bohdanrakov/controllers/ResourceServiceController.java
@@ -4,6 +4,7 @@ import io.github.bohdanrakov.dtos.MP3FileDTO;
 import io.github.bohdanrakov.dtos.MP3StoreRequest;
 import io.github.bohdanrakov.dtos.MP3StoreResponse;
 import io.github.bohdanrakov.services.MP3StorageService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -32,6 +33,7 @@ public class ResourceServiceController {
         MP3FileDTO mp3File = mp3StorageService.getMP3File(id);
         return ResponseEntity
                 .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_TYPE, "audio/mpeg")
                 .body(mp3File.byteContent());
     }
 }
